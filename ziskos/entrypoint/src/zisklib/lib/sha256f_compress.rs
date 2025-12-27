0a54feb28d8a369b938c2ee5c4b19e42d0d18669
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2025-12-27 18:17:55 +0000

    Improving the sha call

--- ziskos/entrypoint/src/zisklib/lib/sha256f_compress.rs
+++ ziskos/entrypoint/src/zisklib/lib/sha256f_compress.rs
@@ -14,23 +14,8 @@ pub unsafe extern "C" fn sha256f_compress_c(
     let state_64: &mut [u64; 4] = &mut *(state_ptr as *mut [u64; 4]);
 
     for i in 0..num_blocks {
-        let block: &[u8; 64] = &*(blocks_ptr.add(i * 64) as *const [u8; 64]);
-        let input_u64 = convert_bytes_to_u64(block);
-        let mut sha256_params = SyscallSha256Params { state: state_64, input: &input_u64 };
+        let input_u64: &[u64; 8] = &*(blocks_ptr.add(i * 64) as *const [u64; 8]);
+        let mut sha256_params = SyscallSha256Params { state: state_64, input: input_u64 };
         syscall_sha256_f(&mut sha256_params);
     }
-}
-
-#[inline(always)]
-fn convert_bytes_to_u64(input: &[u8; 64]) -> [u64; 8] {
-    [
-        u64::from_be_bytes(input[0..8].try_into().unwrap()),
-        u64::from_be_bytes(input[8..16].try_into().unwrap()),
-        u64::from_be_bytes(input[16..24].try_into().unwrap()),
-        u64::from_be_bytes(input[24..32].try_into().unwrap()),
-        u64::from_be_bytes(input[32..40].try_into().unwrap()),
-        u64::from_be_bytes(input[40..48].try_into().unwrap()),
-        u64::from_be_bytes(input[48..56].try_into().unwrap()),
-        u64::from_be_bytes(input[56..64].try_into().unwrap()),
-    ]
 }

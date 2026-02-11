eb8fb1d52b64af6e966d064778b3ffbf74abbdd0
Fran Segovia <agnusmor@gmail.com>
2026-02-11 17:53:15 +0000

    Adding total hints count in print_metrics function

--- ziskos/entrypoint/src/hints/metrics.rs
+++ ziskos/entrypoint/src/hints/metrics.rs
@@ -1,3 +1,4 @@
+use num_traits::float::TotalOrder;
 use once_cell::sync::Lazy;
 use std::{collections::HashMap, sync::RwLock};
 
@@ -27,10 +28,13 @@ pub(crate) fn inc_hint_count(hint_id: u32) {
 
 pub(crate) fn print_metrics() {
     let hints = crate::hints::metrics::HINTS_METRICS.read().expect("HINTS_METRICS poisoned");
+    let mut total_hints = 0;
     println!("Hints usage summary:");
     for (_, info) in hints.iter() {
         if info.count > 0 {
             println!("  {}: {}", info.hint_name, info.count);
         }
+        total_hints += info.count;
     }
+    println!("Total hints: {}", total_hints);
 }

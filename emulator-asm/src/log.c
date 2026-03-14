1cd0ee464cf77271983a99082f6c11c381ada223
fractasy <fractasy@gmail.com>
2026-03-14 19:31:18 +0100

    Add date and time to assembly log

--- emulator-asm/src/log.c
+++ emulator-asm/src/log.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 #include "log.hpp"
 #include "globals.hpp"
 
@@ -8,8 +9,16 @@ void asm_printf(const char *format, ...)
     // Flush any previous error message
     fflush(stderr);
 
+    // Get current date and time
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+    
+    // Custom format: YYYY-MM-DD HH:MM:SS
+    char date_and_time[80];
+    strftime(date_and_time, sizeof(date_and_time), "%Y-%m-%d %H:%M:%S", tm_info);
+
     // Print the prefix first
-    printf("[ASM %s] ", log_name);
+    printf("[ASM %s %s] ", log_name, date_and_time);
     
     // Handle the variable arguments
     va_list args;
